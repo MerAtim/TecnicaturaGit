82416b75167bacd7f7dd5843aa8c6c663d827b5e
MARIA MERCEDES GUERRA <127444671+MerAtim@users.noreply.github.com>
2024-06-19 03:00:30 -0300

    Agrego carpeta Javascript y ejercicios de la segunda clase de Javascript

---
+++ Laboratorio Python-Javascript/Javascript/Clase 2/01-01-HolaMundo.js
@@ -0,0 +1,8 @@
+var nombre = "Mercedes";
+nombre = "Maria \nMercedes";
+var apellido = "\nAtim"
+console.log(nombre, apellido);
+
+
+// node "C:\Users\MyL\Desktop\TecnicaturaUTN\Laboratorio Python-Javascript\Javascript\Clase 2\01-01-HolaMundo.js"
+//Lo anterior es para poder ejecutar en la terminal de VSC
---
+++ Laboratorio Python-Javascript/Javascript/Clase 2/HolaMundo.js
@@ -0,0 +1,3 @@
+var nombre = "Mercedes";
+nombre = "Hola Mundo desde Javascript";
+console.log(nombre);
