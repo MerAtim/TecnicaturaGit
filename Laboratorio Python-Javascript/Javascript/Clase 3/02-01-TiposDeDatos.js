0c6b8f2b866facc83a0fd55a007337b9b02a9bd7
MARIA MERCEDES GUERRA <127444671+MerAtim@users.noreply.github.com>
2024-06-19 04:36:11 -0300

    Agrego ejercicio de la clase 3 de Javascript

---
+++ Laboratorio Python-Javascript/Javascript/Clase 3/02-01-TiposDeDatos.js
@@ -0,0 +1,8 @@
+var nombre = "Mercedes"; //String
+console.log(nombre);
+
+var numero = 3000; //Numerico
+console.log(numero)
+
+var objeto = {nombre: "Mercedes", apellido: "Atim", telefono: "12345678"} //objeto
+console.log(objeto);
